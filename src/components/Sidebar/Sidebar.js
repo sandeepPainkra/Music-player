835d549d2fff51d32c2ea3737508d0634ba28391
Sandeep Painkra <samdeeppainkra@gmail.com>
2022-05-12 21:53:54 +0530

    sidebarupdate

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,6 +7,7 @@ import { IoLibrary } from "react-icons/io5";
 import apiClient from "../../spotify";
 
 const Sidebar = () => {
+  const [user, setUser] = useState([]);
   const [image, setImage] = useState(
     "https://scontent.fidr1-1.fna.fbcdn.net/v/t1.6435-9/79471106_101080374734128_7878052739169648640_n.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=dJ7Kt2JP7F0AX9UhBTy&_nc_ht=scontent.fidr1-1.fna&oh=00_AT_gy6Dk0gs8FLs0VhqHlQLdPNrCPhb_Esmr2LQmGrsRoQ&oe=629C0095"
   );
@@ -15,8 +16,10 @@ const Sidebar = () => {
     apiClient.get("me").then((response) => {
       console.log(response.data.images[0].url);
       setImage(response.data.images[0].url);
+      setUser(response);
     });
   }, []);
+  console.log(user);
 
   return (
     <div className="sidebar_container">
